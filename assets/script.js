d37d2429784f249a2596db9a25a5fd2ee4e3c807
jwputah <jwputah7@gmail.com>
2022-01-26 22:46:49 -0700

    js code to display objects on page

--- assets/script.js
+++ assets/script.js
@@ -9,18 +9,16 @@ function getApi() {
 		"x-rapidapi-key": "bf92c9263fmsh9b5ba3341c5c1abp151886jsn998a30f334a8"
 	}
 })
-  
     .then(function (response) {
         return response.json();
     
     })
     .then(function (data) {
-        // Use the console to examine the response
-        console.log(data);
 
-    //     var listItem = document.createElement('h3');
-    //     listItem.textContent = data;
-    //     showJoke.appendChild(listItem);
+
+        var listItem = JSON.stringify(data);
+        listItem = document.getElementById("dadJoke").innerHTML = listItem;
+        console.log(listItem);
     });
 }
 submitBtn.addEventListener('click', getApi);
