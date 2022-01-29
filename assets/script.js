018f39558a5f7b5dd6c0570f280129a7274a0d52
jwputah <jwputah7@gmail.com>
2022-01-28 22:02:45 -0700

    new api and code working to display joke only

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,19 @@
 
-var showJoke = document.getElementById("dadJoke");
-
 function getApi() {
-    fetch("https://dad-jokes.p.rapidapi.com/random/joke", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "dad-jokes.p.rapidapi.com",
-		"x-rapidapi-key": "bf92c9263fmsh9b5ba3341c5c1abp151886jsn998a30f334a8"
-	}
-})
+    fetch("https://icanhazdadjoke.com/", {
+	    headers: {
+        Accept: "application/json",
+	    }
+    })
     .then(function (response) {
         return response.json();
-    
     })
     .then(function (data) {
-
+        console.log(data);
 
         var listItem = JSON.stringify(data);
-        listItem = document.getElementById("dadJoke").innerHTML = listItem;
+        listItem = document.getElementById("dadJoke").innerHTML = data.joke;
         console.log(listItem);
-    });
+    })
 }
 newJoke.addEventListener('click', getApi);
