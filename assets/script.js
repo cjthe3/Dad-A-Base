86890b2523a2acb21b9ae3feb14205794f203090
jwputah <jwputah7@gmail.com>
2022-01-31 19:57:02 -0700

    added bulletin points to list and joke centered

--- assets/script.js
+++ assets/script.js
@@ -2,42 +2,39 @@
 
 function getApi() {
     fetch("https://icanhazdadjoke.com/", {
-	    headers: {
-        Accept: "application/json",
-	    }
+        headers: {
+            Accept: "application/json",
+        }
     })
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
 
-        var listItem = JSON.stringify(data);
-        listItem = document.getElementById("dadJoke").innerHTML = data.joke;
-        console.log(listItem);
-    })
+            var listItem = JSON.stringify(data);
+            listItem = document.getElementById("dadJoke").innerHTML = data.joke;
+            console.log(listItem);
+        })
 }
 newJoke.addEventListener("click", getApi);
-
 var favoriteBtn = document.getElementById("favejoke");
-
 favoriteBtn.addEventListener("click", saveFavorite);
 
-
-function saveFavorite () {
+function saveFavorite() {
     //getting text content from current joke
     var currentJokeElement = document.getElementById("dadJoke");
     var currentJokeText = currentJokeElement.textContent;
     //reading previous saved jokes
     var stringifiedArray = localStorage.getItem("savedJokes");
     //treat null as empty array
-    if  (stringifiedArray === null) {
-        stringifiedArray  = "[]";
+    if (stringifiedArray === null) {
+        stringifiedArray = "[]";
     }
     //changing from string to array
     var parsedArray = JSON.parse(stringifiedArray);
     //appending current joke to favorite list
-    parsedArray.push (currentJokeText);
+    parsedArray.push(currentJokeText);
     //changing from array to string
     stringifiedArray = JSON.stringify(parsedArray);
     //saved to local storage
@@ -54,8 +51,8 @@ function initializeFavorites() {
     var jokeListElement = document.getElementById("favoritedJokes");
     var stringifiedArray = localStorage.getItem("savedJokes");
     //treat null as empty array
-    if  (stringifiedArray === null) {
-        stringifiedArray  = "[]";
+    if (stringifiedArray === null) {
+        stringifiedArray = "[]";
     }
     //changing from string to array
     var parsedArray = JSON.parse(stringifiedArray);
@@ -66,5 +63,4 @@ function initializeFavorites() {
         jokeListElement.appendChild(listItem);
     }
 }
-
 initializeFavorites();
